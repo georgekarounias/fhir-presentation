08e7225bdd7659baba6037de8b738bbf98f5d4cc
George Karounias <george.karounias@gmail.com>
2023-05-01 03:10:58 +0300

    add info message for chart

--- src/pages/Meassurements/HRPage/HRPage.tsx
+++ src/pages/Meassurements/HRPage/HRPage.tsx
@@ -5,14 +5,19 @@ import ObservationsFilters, {
 import { useState } from "react";
 import { getPatientsObesrvations } from "../../../FhirServices/FhirServices";
 import { Observation } from "fhir/r4";
-import Appsettings from '../../../helpers/AppSettings';
-import SingleValueChart, { IDataPoint } from "../../../components/Charts/SingleValueChart";
+import Appsettings from "../../../helpers/AppSettings";
+import SingleValueChart, {
+  IDataPoint,
+} from "../../../components/Charts/SingleValueChart";
+import { Space } from "antd";
+import { convertDatetoIsoStringWithoutTimeZone } from "../../../helpers/DateHandler";
 
 const HRPage = () => {
   const { t } = useTranslation();
 
   const [loading, setLoading] = useState<boolean>(false);
   const [hrObservations, setHrObservations] = useState<Observation[]>([]);
+  const [parentFilterState, setParentFilterState] = useState<IObservationState|undefined>(undefined);
 
   const searchResources = (filterState: IObservationState) => {
     if (
@@ -22,10 +27,11 @@ const HRPage = () => {
     ) {
       return;
     }
+    setParentFilterState(filterState);
     getPatientsObesrvations(
       filterState.patientId,
       filterState.startDate,
-      filterState.endDate, 
+      filterState.endDate,
       Appsettings.HeartRateCode
     ).then((res) => {
       setHrObservations(res);
@@ -34,17 +40,28 @@ const HRPage = () => {
 
   return (
     <div>
-      <h1>{t("heartRatePage.header")}</h1>
-      <ObservationsFilters
-        onSubmitFilters={(filterState) => searchResources(filterState)}
-      />
-      {hrObservations?.length &&<SingleValueChart data={hrObservations.map(x=> {
-        const datapoint: IDataPoint = {
-          time : x.effectiveDateTime ?? "",
-          value : x.valueQuantity?.value ?? 0
-        }
-        return datapoint
-      })}/>}
+      <Space direction="vertical" size={20}>
+        <h1>{t("heartRatePage.header")}</h1>
+        <ObservationsFilters
+          onSubmitFilters={(filterState) => searchResources(filterState)}
+        />
+        {hrObservations?.length>0 && (
+          <div>
+            {t("infoMessage").replace("_recordsLength_", hrObservations.length.toString()).replace("_userId_", parentFilterState?.patientId?.toString() ?? "_").replace("_startDate_", parentFilterState?.startDate ? convertDatetoIsoStringWithoutTimeZone(parentFilterState?.startDate).slice(0,10) :"_").replace("_endDate_", parentFilterState?.endDate ? convertDatetoIsoStringWithoutTimeZone(parentFilterState?.endDate).slice(0,10) :"_")}
+          </div>
+        )}
+        {hrObservations?.length > 0 && (
+          <SingleValueChart
+            data={hrObservations.map((x) => {
+              const datapoint: IDataPoint = {
+                time: x.effectiveDateTime ?? "",
+                value: x.valueQuantity?.value ?? 0,
+              };
+              return datapoint;
+            })}
+          />
+        )}
+      </Space>
     </div>
   );
 };
