7a631b4218efd5f6b5ca1c663414ab3ff079aca2
George Karounias <george.karounias@gmail.com>
2023-05-02 12:19:47 +0300

    fix params for translation

--- src/helpers/Messaging.ts
+++ src/helpers/Messaging.ts
@@ -8,16 +8,16 @@ export const setInfoMessagge = (
   endDate?: Date
 ) => {
   const msg = messageBase
-    .replace("_recordsLength_", recordsCount.toString())
-    .replace("_userId_", userId?.toString() ?? "_")
+    .replace("_0_", recordsCount.toString())
+    .replace("_1_", userId?.toString() ?? "_")
     .replace(
-      "_startDate_",
+      "_2_",
       startDate
         ? convertDatetoIsoStringWithoutTimeZone(startDate).slice(0, 10)
         : "_"
     )
     .replace(
-      "_endDate_",
+      "_3_",
       endDate
         ? convertDatetoIsoStringWithoutTimeZone(endDate).slice(0, 10)
         : "_"
